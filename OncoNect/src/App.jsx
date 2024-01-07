b24b911f16ea8221ce7e6255eea3496e9f3dc417
Christopher Arthaud <christopher.arthaud@gmail.com>
2024-01-07 15:54:39 +0100

    First version of the app

--- OncoNect/src/App.jsx
+++ OncoNect/src/App.jsx
@@ -1,22 +1,20 @@
-import { useState } from "react";
+import styles from "./App.module.css";
 import Slider from "./Slider";
 
 function App() {
   return (
     <>
       <header>
-        <h1>Bienvenue dans votre espace OncoNect</h1>
+        <h1>Recrutement stage OncoNect</h1>
       </header>
       <main>
         <h2>
           Sur une échelle de 0 à 10 quel est votre ressenti à propos des
           symptomes suivants ?
         </h2>
-        <Slider
-          src="../src/assets/mal-de-crane.png"
-          title="mal à la tête"
-          name="headHache"
-        />
+        <Slider title="Maux de tête" name="headhache" />
+        <Slider title="Difficultés à respirer" name="breathless" />
+        <Slider title="Perte d'appétit" name="appetite" />
       </main>
       <footer>Christopher Arthaud - 2024</footer>
     </>
--- OncoNect/src/Slider.jsx
+++ OncoNect/src/Slider.jsx
@@ -1,22 +1,31 @@
 import { useState } from "react";
 import styles from "./Slider.module.css";
 
-function Slider({ src, title, name }) {
+function Slider({ title, name }) {
   const [value, setValue] = useState(0);
+  const [colors, setColors] = useState(0);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+    setColors(`hue-rotate(${-1.5 * e.target.value}deg)`);
+  };
 
   return (
-    <>
-      <label className={styles.sliding} htmlFor={name}>
-        {title}
-      </label>
+    <div id={styles.sliderContainer}>
+      <label htmlFor={name}>{title}</label>
       <input
         className={styles.sliding}
+        id={styles[name]}
         type="range"
         value={value}
+        min={0}
+        max={100}
+        defaultValue={0}
         name={name}
-        onChange={(e) => setValue(e.target.value)}
+        onChange={(e) => handleChange(e)}
+        style={{ filter: colors }}
       ></input>
-    </>
+    </div>
   );
 }
 
